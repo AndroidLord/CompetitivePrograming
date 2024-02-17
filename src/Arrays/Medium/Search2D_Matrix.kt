44ed3719805c1bbb7466de169981c92f0687f953
Shubham Singh Bisht <shubham8318461615@gmail.com>
2024-02-17 09:14:53 +0530

    Solved Search 2D Matrix in Kotlin

--- src/Arrays/Medium/Search2D_Matrix.kt
+++ src/Arrays/Medium/Search2D_Matrix.kt
@@ -1,9 +1,18 @@
-public class Search2D_Matrix {
+class Solution {
+    fun searchMatrix(matrix: Array<IntArray>, target: Int): Boolean {
+       
+       val r = matrix.size
+       val c = matrix[0].size
 
-    public static void main(String[] args) {
-        // Your code here
+       var low = 0
+       var high = c-1
+       
+       while(low<r && high>-1){
+           val cur = matrix[low][high]
+           if(cur==target) return true
+           else if(target>cur)low++
+           else high--
+       }
+       return false
     }
-
-    
-
 }
